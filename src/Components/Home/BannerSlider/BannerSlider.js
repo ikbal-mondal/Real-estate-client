896335572333e3c78d7570f8e930863698db6006
ikbal-mondal <kbalmondal062@gmail.com>
2022-12-22 09:32:20 +0530

    added details page

--- src/Components/Home/BannerSlider/BannerSlider.js
+++ src/Components/Home/BannerSlider/BannerSlider.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from "swiper/react";
-
 import "swiper/css";
 import "swiper/css/pagination";
 import { Pagination } from 'swiper';
+import banner1 from '../../../assest/banner1.jpg'
+import banner2 from '../../../assest/banner2.jpg'
+import banner3 from '../../../assest/banner3 .jpg'
+
+
 const BannerSlider = () => {
     return (
         <div>
@@ -15,9 +19,9 @@ const BannerSlider = () => {
         modules={[Pagination]}
         className="mySwiper"
       >
-        <SwiperSlide><img className='w-full h-96' src="https://ap.rdcpix.com/f9069154b66f990edad57a3480471167l-m1230024412od-w480_h360_x2.jpg" alt="" /></SwiperSlide>
-        <SwiperSlide><img className='w-full h-96' src="https://nh.rdcpix.com/330615e436757ed52a9aa74387fdf4del-f623634504od-w480_h360_x2.webp" alt="" /></SwiperSlide>
-        <SwiperSlide><img className='w-full h-96' src="https://ap.rdcpix.com/69be20ef1ebbfe3aca2e89f5af4ef4d9l-m3363214368od-w480_h360_x2.webp" alt="" /></SwiperSlide>
+        <SwiperSlide><img className='w-full ' src={banner1} alt="" /></SwiperSlide>
+        <SwiperSlide><img className='w-full ' src={banner2} alt="" /></SwiperSlide>
+        <SwiperSlide><img className='w-full ' src={banner3} alt="" /></SwiperSlide>
        
       </Swiper>
         </div>
--- src/Components/Home/category/DetailsShow.js
+++ src/Components/Home/category/DetailsShow.js
@@ -3,11 +3,53 @@ import { useLoaderData } from 'react-router-dom';
 
 const DetailsShow = () => {
     const data = useLoaderData()
-    const {name} = data
+    const 
+    {  name,
+        url,
+        price,
+        sqft,
+        locality_name,
+        Bedrooms,
+        bathrooms,
+        Garage,
+        Built,
+        Kitchen,
+        Description
+    } = data
     return (
         <div>
-            <h1>details page</h1>
-            <p>{name}</p>
+           <div className=" mx-auto  text-gray-900">
+	<div className="flex flex-col max-w-8xl  mx-auto overflow-hidden rounded">
+		<img style={{height:'500px'}} src={url} alt="" className="w-full  bg-gray-100" />
+		<div className="mx-auto">
+        <div className="p-6 pb-12 m-4 mx-auto -mt-16 space-y-6 lg:max-w-5xl   sm:px-10 sm:mx-12 lg:rounded-md bg-gray-100">
+        <span className='bg-green-300 px-2 rounded  text-green-600'>sale</span>
+			<div className="">
+            
+				<h2 className="inline-block space-y-2 text-2xl font-semibold sm:text-4xl">{name}</h2>
+				<p className='my-2 text-xl text-slate-600'><i class="fa-solid fa-location-dot"></i> {locality_name}</p>
+                <h2 className='text-4xl my-2 font-bold text-sky-600'>$: {price}</h2>
+                <div className="grid lg:grid-cols-3 my-4 xs:grid-cols-2 md:grid-cols-3 align-middle gap-4 items-center sm:grid-cols-2">
+            <p className='text-slate-600'><i class="fa-solid bg-slate-300 px-2 py-1 rounded text-slate-500 fa-vector-square"></i> {sqft} sqft</p>
+            <p className='text-slate-600'><i class="fa-solid bg-slate-300 px-2 py-1 rounded text-slate-500 fa-location-dot"></i> {locality_name}</p>
+            <p className='text-slate-600'><i class="fa-solid bg-slate-300 px-2 py-1 rounded text-slate-500 fa-bed"></i> {Bedrooms}</p>
+            <p className='text-slate-600'><i class="fa-solid bg-slate-300 px-2 py-1 rounded text-slate-500 fa-bath"></i> {bathrooms}</p>
+            <p className='text-slate-600'><i class="fa-solid bg-slate-300 px-2 py-1 rounded text-slate-500 fa-car"></i> {Garage}</p>
+            <p className='text-slate-600'><i class="fa-solid bg-slate-300 px-2 py-1 rounded text-slate-500 fa-kitchen-set"> </i> {Kitchen} Kitchen </p>
+            <p className='text-slate-600'><i class="fa-solid bg-slate-300 px-2 py-1 rounded text-slate-500  fa-building-circle-check"> </i> {Built} Built </p>
+           </div>
+           <div className="my-4 mt-8">
+            <span className='text-lg font-semibold my-1'>Description:</span>
+            <p>
+                {Description}
+            </p>
+           </div>
+			</div>
+			
+		</div>
+        </div>
+	</div>
+</div>
         </div>
     );
 };
